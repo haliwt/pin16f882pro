db24866cf23bcdaf8738cda71f4f779fe6956c02
haliwt <jonewang@126.com>
2022-04-13 08:10:18 +0800

    To add timer 0 function DATA.2022.04.13

--- hardware/inc/dht11.h
+++ hardware/inc/dht11.h
@@ -2,10 +2,29 @@
 #define __DHT11_H_
 #include "../../main.h"
 
-void DTH11_Init(void);
+#define _XTAL_FREQ  8000000
+
+#define DHT11_RA5_GetValue()           PORTAbits.RA5
+#define DHT11_DATA_BIT()               PORTAbits.RA5 
+
+typedef unsigned char uint8_t ;
 
 
+typedef struct{
 
+    unsigned char Humidity_high;
+    unsigned char Humidify_low;
+    unsigned char Temperature_high;
+    unsigned char Temperature_low;
+    unsigned char DHT11_CAC;
+
+
+}DHT11_info;
+
+void DHT11_Init(void);
+
+
+void DHT11_ReadInfo_Value(DHT11_info *temp);
 
 
 
--- hardware/src/dht11.c
+++ hardware/src/dht11.c
@@ -1,1 +1,65 @@
 #include "../inc/dht11.h"
+
+static uint8_t DHT11ReadBuyte(void);
+
+void DHT11_Init(void)
+{
+
+
+
+
+
+}
+
+
+
+void DHT11_ReadInfo_Value(DHT11_info *temp)
+{
+
+    temp->Humidity_high  = DHT11ReadBuyte();
+    temp->Humidify_low   = DHT11ReadBuyte();
+
+    temp->Temperature_high = DHT11ReadBuyte();
+    temp ->Temperature_low   =  DHT11ReadBuyte();
+
+    temp->DHT11_CAC = DHT11ReadBuyte();
+
+
+
+}
+
+static uint8_t DHT11ReadBuyte(void)
+{
+    uint8_t j=0;
+    uint8_t i,datCode;
+
+    for(i=0;i<8;i++){
+
+        datCode <<=1;
+        j=0;
+
+        while((!DHT11_DATA_BIT()) && (j<250)){
+
+            j++;
+            
+        }
+        __delay_us(10);
+        __delay_us(10);
+        __delay_us(10);
+        __delay_us(10);
+        if(DHT11_DATA_BIT()){
+
+             datCode |= 0x01;
+             j=0;
+             while((DHT11_DATA_BIT()) &&(j<250)){
+
+                 j++;
+             }
+        }
+    }
+
+    return datCode;
+
+
+
+}
